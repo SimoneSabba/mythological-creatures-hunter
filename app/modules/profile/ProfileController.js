e10765ddbdd6430b4f75c526c5ca33ddf3e6d430
Simone Sabbatini <sabbatini.simone@gmail.com>
2016-09-13 15:04:40 +0100

    added profile page (to babe completed...)

--- app/modules/profile/ProfileController.js
+++ app/modules/profile/ProfileController.js
@@ -1,9 +1,21 @@
 'use strict';
 
-function ProfileCtrl($scope, $state) {
+function ProfileCtrl($scope, $state, ProfileService) {
 
-    
+	var init = function() {
+			$scope.profileInfo = ProfileService.getMyInfo();	
+	}
+
+	$scope.free = function(creature) {
+			$scope.profileInfo = ProfileService.removeCreature(creature);
+	}
+
+	$scope.huntMore = function() {
+			$state.go('home');
+	}
+
+		init();
 }
 
-ProfileCtrl.$inject = ['$scope', '$state'];
+ProfileCtrl.$inject = ['$scope', '$state', 'ProfileService'];
 module.exports = ProfileCtrl;
