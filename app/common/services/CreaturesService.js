7b7b3d60cc25e33f7b9f392ba23567c0cc978541
Simone Sabbatini <sabbatini.simone@gmail.com>
2016-09-14 11:30:04 +0100

    added creature pictures

--- app/common/services/CreaturesService.js
+++ app/common/services/CreaturesService.js
@@ -3,18 +3,18 @@
 var CreaturesService = function(CONSTANTS) {
     
     var creatures = [];
-    creatures[0] = { type: CONSTANTS.PHOENIXE, image: '' };
-    creatures[1] = { type: CONSTANTS.MEDUSA, image: '' };
-    creatures[2] = { type: CONSTANTS.HIPPOGRIFF, image: '' };
-    creatures[3] = { type: CONSTANTS.UNICORN, image: '' };
-    creatures[4] = { type: CONSTANTS.CENTAUR, image: '' };
-    creatures[5] = { type: CONSTANTS.DAEMON, image: '' };
-    creatures[6] = { type: CONSTANTS.VAMPIRE, image: '' };
-    creatures[7] = { type: CONSTANTS.MINOTAUR, image: '' };
-    creatures[8] = { type: CONSTANTS.WEREWOLF, image: '' };
-    creatures[9] = { type: CONSTANTS.PEGASUS, image: '' };
-    creatures[10] = { type: CONSTANTS.CHIMERA, image: '' };
-    creatures[11] = { type: CONSTANTS.YETI, image: '' };
+    creatures[0] = { type: CONSTANTS.PHOENIXE, image: 'images/phoenix.jpg' };
+    creatures[1] = { type: CONSTANTS.MEDUSA, image: 'images/medusa.jpg' };
+    creatures[2] = { type: CONSTANTS.HIPPOGRIFF, image: 'images/hyppogriff.jpg' };
+    creatures[3] = { type: CONSTANTS.UNICORN, image: 'images/unicorn.jpg' };
+    creatures[4] = { type: CONSTANTS.CENTAUR, image: 'images/centaur.jpg' };
+    creatures[5] = { type: CONSTANTS.DAEMON, image: 'images/daemon.jpg' };
+    creatures[6] = { type: CONSTANTS.VAMPIRE, image: 'images/vampire.jpg' };
+    creatures[7] = { type: CONSTANTS.MINOTAUR, image: 'images/minotaur.jpg' };
+    creatures[8] = { type: CONSTANTS.WEREWOLF, image: 'images/werewolf.jpeg' };
+    creatures[9] = { type: CONSTANTS.PEGASUS, image: 'images/pegasus.jpg' };
+    creatures[10] = { type: CONSTANTS.CHIMERA, image: 'images/chimera.jpg' };
+    creatures[11] = { type: CONSTANTS.YETI, image: 'images/yeti.png' };
 
     this.getCreatures = function() {
         return creatures;
